f5b2517f5e6daf081d0abe274f132d491028459d
Daniel Beck <daniel-beck@users.noreply.github.com>
2023-08-31 09:42:52 +0200

    Fix deprecation warning

--- src/test/java/io/jenkins/plugins/environment_filter_utils/matchers/ExactJobFullNameRunMatcherTest.java
+++ src/test/java/io/jenkins/plugins/environment_filter_utils/matchers/ExactJobFullNameRunMatcherTest.java
@@ -33,7 +33,7 @@ import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.MockFolder;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ExactJobFullNameRunMatcherTest {
 
